e011c1ed5313be797cbffc957ef673f2d54b2750
Nikhil Bahubali <nikhilba@yahoo-inc.com>
2017-01-16 21:34:20 -0800

    added database serialization support

---
+++ app/src/main/java/com/yahoo/sports/todoapp/ToDoApplication.java
@@ -0,0 +1,21 @@
+package com.yahoo.sports.todoapp;
+
+import android.app.Application;
+
+import com.raizlabs.android.dbflow.config.FlowConfig;
+import com.raizlabs.android.dbflow.config.FlowManager;
+
+/**
+ * Created by nikhilba on 1/16/17.
+ */
+
+public class ToDoApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // This instantiates DBFlow
+        FlowManager.init(new FlowConfig.Builder(this).build());
+        // add for verbose logging
+        // FlowLog.setMinimumLoggingLevel(FlowLog.Level.V);
+    }
+}
---
+++ app/src/main/java/com/yahoo/sports/todoapp/ToDoItem.java
@@ -0,0 +1,37 @@
+package com.yahoo.sports.todoapp;
+
+import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+
+/**
+ * Created by nikhilba on 1/16/17.
+ */
+
+@Table(database = ToDoAppDatabase.class)
+public class ToDoItem extends BaseModel {
+
+    @Column
+    @PrimaryKey
+    public int id;
+
+    @Column
+    public String itemName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+}
